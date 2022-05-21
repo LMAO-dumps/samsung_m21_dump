52a140ad174931c98261188c32639f6b93c6722c
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2022-05-21 10:41:19 +0000

    Add extras for m21nsxx-user 12 SP1A.210812.016 M215FXXU2CVCC release-keys
    Signed-off-by: LMAO-armv8 <chaitanya4g9@sasi.ac.in>

---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/framework/oat/arm64/services.odex.* 2>/dev/null >> system/system/framework/oat/arm64/services.odex
+rm -f system/system/framework/oat/arm64/services.odex.* 2>/dev/null
+cat system/system/framework/framework-res.apk.* 2>/dev/null >> system/system/framework/framework-res.apk
+rm -f system/system/framework/framework-res.apk.* 2>/dev/null
+cat system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null >> system/system/priv-app/AREmoji/AREmoji.apk
+rm -f system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null >> system/system/app/HoneyBoard/HoneyBoard.apk
+rm -f system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null
